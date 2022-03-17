9043872398b1cc44fa3af86a8e9557128d843181
JLMaikl <jlmaikl@mail.ru>
2022-03-17 08:17:07 +0300

    Add fitch sign in/sign out

--- project/src/components/user-block/user-block.tsx
+++ project/src/components/user-block/user-block.tsx
@@ -1,13 +1,14 @@
 // import { Link } from 'react-router-dom';
-import { APIRoute } from '../../const';
-import { useAppDispatch } from '../../hooks';
+import { APIRoute, AppRoute, AuthorizationStatus } from '../../const';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import { logoutAction } from '../../store/api-actions';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 
 function UserBlock() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
 
 
   const handleUserLogOutClick = () => {
@@ -17,20 +18,28 @@ function UserBlock() {
 
   return (
     <ul className='user-block'>
-      <li className='user-block__item'>
-        <div className='user-block__avatar'>
-          <img
-            src='img/avatar.jpg'
-            alt='User avatar'
-            width='63'
-            height='63'
-          />
-        </div>
-      </li>
-      <li className='user-block__item'>
-        {/* <Link to={AppRoute.Login} onClick={(evt) => { evt.preventDefault(); dispatch(logoutAction());}} className='user-block__link'>Sign out</Link> */}
-        <span className="user-block__link" onClick={handleUserLogOutClick}>Sign out</span>
-      </li>
+      {
+        authorizationStatus === AuthorizationStatus.Auth ?
+          <>
+            <li className='user-block__item'>
+              <div className='user-block__avatar'>
+                <img
+                  src='img/avatar.jpg'
+                  alt='User avatar'
+                  width='63'
+                  height='63'
+                />
+              </div>
+            </li>
+            <li className='user-block__item'>
+              <span className="user-block__link" onClick={handleUserLogOutClick}>Sign out</span>
+            </li>
+          </>
+          :
+          <li className='user-block__item'>
+            <Link to={AppRoute.Login}className="user-block__link" >Sign in</Link>
+          </li>
+      }
     </ul >
   );
 }
